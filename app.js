a92303d30d6cdd70c9cee295aed3872956b9ecab
ailinmino <pukyailin@gmail.com>
2024-08-15 23:39:40 -0300

    se instaló cors para usar las rutas

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express, { json } from 'express';
 import taskRoutes from './routes/taskRoutes.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(json());
 
